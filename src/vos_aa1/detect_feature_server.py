5560aec1c6c1375e82f765d193c04a0120cdb054
William Chamberlain <william.chamberlain@hdr.qut.edu.au>
2017-03-07 16:10:45 +1000

    Renamed VisualFeature to VisualFeatureObservation. Created VisualFeatureInWorld with Pose rather than PoseStamped. Created LocaliseFromAFeature service - not implemented yet.

--- src/vos_aa1/detect_feature_server.py
+++ src/vos_aa1/detect_feature_server.py
@@ -1,11 +1,36 @@
 #!/usr/bin/env python
 
+import rospy
+import tf
+from geometry_msgs.msg import Pose
+
 from vos_aa1.srv import DetectedFeature
 from vos_aa1.srv import DetectedFeatureRequest
 from vos_aa1.srv import DetectedFeatureResponse
-from vos_aa1.msg import VisualFeature
-import rospy
-import tf
+from vos_aa1.srv import LocaliseFromAFeature
+from vos_aa1.msg import VisualFeatureInWorld
+from vos_aa1.msg import VisualFeatureObservation
+
+algorithm_abreviations = {'AprilTags_Kaess_36h11':'t'}
+
+
+fixed_features = []
+tag_55 = VisualFeatureInWorld()
+tag_55.algorithm = 'AprilTags_Kaess_36h11'
+tag_55.id = '55'
+tag_55.pose = Pose()
+tag_55.pose.position.x=0
+tag_55.pose.position.y=0
+tag_55.pose.position.z=0.8
+tag_55.pose.orientation.x=0
+tag_55.pose.orientation.y=0
+tag_55.pose.orientation.z=0
+tag_55.pose.orientation.w=1
+fixed_features.append(tag_55)
+
+def localise_from_a_feature_callback(req):
+    print "----------------------------------------------"
+    print "localise_from_a_feature_callback: "
 
 def detect_feature_callback(req):
     print "----------------------------------------------"
@@ -15,7 +40,7 @@ def detect_feature_callback(req):
     print "  camera orientation (quaternion) [%.4f,%.4f,%.4f,%.4f] : "%(req.cameraPose.pose.orientation.x,req.cameraPose.pose.orientation.y,req.cameraPose.pose.orientation.z,req.cameraPose.pose.orientation.w)
     print "  feature algorithm [%s] : "%(req.visualFeature.algorithm)
     print "  feature id [%d] : "%(req.visualFeature.id)
-    print '  feature frame_id : %s_%s%s' % (req.cameraPose.header.frame_id, req.visualFeature.algorithm, req.visualFeature.id)
+    print '  feature frame_id : %s_%s%s' % (req.cameraPose.header.frame_id, algorithm_abreviations[req.visualFeature.algorithm], req.visualFeature.id)
     print "  feature translation [%.4f,%.4f,%.4f] : "%(req.visualFeature.pose.pose.position.x, req.visualFeature.pose.pose.position.y, req.visualFeature.pose.pose.position.z)
     print "  feature orientation (quaternion) [%.4f,%.4f,%.4f,%.4f] "%(req.visualFeature.pose.pose.orientation.x, req.visualFeature.pose.pose.orientation.y, req.visualFeature.pose.pose.orientation.z, req.visualFeature.pose.pose.orientation.w)
 
@@ -55,8 +80,18 @@ def detect_feature_callback(req):
         (req.visualFeature.pose.pose.position.x, req.visualFeature.pose.pose.position.y, req.visualFeature.pose.pose.position.z),
         (req.visualFeature.pose.pose.orientation.x, req.visualFeature.pose.pose.orientation.y, req.visualFeature.pose.pose.orientation.z, req.visualFeature.pose.pose.orientation.w),
         time_now,
-        '%s_%s%s' % (req.cameraPose.header.frame_id, req.visualFeature.algorithm, req.visualFeature.id),  # to   e.g. c1_t1 '/feature/%s/%s/pose' % algorithm, req.visualFeature.id   e.g. "/feature/t/1"     # TODO - remove hardcoding to base namespace
+        '%s_%s%s' % (req.cameraPose.header.frame_id, algorithm_abreviations[req.visualFeature.algorithm], req.visualFeature.id),  # to   e.g. c1_t1 '/feature/%s/%s/pose' % algorithm, req.visualFeature.id   e.g. "/feature/t/1"     # TODO - remove hardcoding to base namespace
         req.cameraPose.header.frame_id + 'zy')            # from      '/cam/%s/pose' % req.cameraPose.header.frame_id e.g. "/cam/c_1/pose"    # TODO - remove hardcoding to base namespace
+
+
+    for fixed_feature in fixed_features:
+        tfBroadcaster.sendTransform(
+            (fixed_feature.pose.position.x, fixed_feature.pose.position.y, fixed_feature.pose.position.z ),
+            (fixed_feature.pose.orientation.x, fixed_feature.pose.orientation.y, fixed_feature.pose.orientation.z, fixed_feature.pose.orientation.w),
+            time_now,
+            '%s%s' % (algorithm_abreviations[fixed_feature.algorithm], fixed_feature.id),
+            'map')
+
 
     response = DetectedFeatureResponse()
     response.acknowledgement="bob"
@@ -65,8 +100,10 @@ def detect_feature_callback(req):
 
 def detect_feature_server():
     rospy.init_node('detect_feature_server')
-    s = rospy.Service('/androidvosopencvros/detected_feature', DetectedFeature, detect_feature_callback)
-    print "Ready to receive detected features. c"
+    detect_feature_server = rospy.Service('/androidvosopencvros/detected_feature', DetectedFeature, detect_feature_callback)
+    print "Ready to receive detected features."
+    localise_from_a_feature_server = rospy.Service('/androidvosopencvros/localise_from_a_feature', LocaliseFromAFeature, localise_from_a_feature_callback)
+    print "Ready to receive localise from individual features."
     rospy.spin()
 
 
